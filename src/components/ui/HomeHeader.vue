e6ff7519033b70a2a5082f050d495e6b4b5e6b30
Daniel Josh <danieludumajosh@gmail.com>
2025-10-29 04:02:17 +0100

    fixes

--- src/components/ui/HomeHeader.vue
+++ src/components/ui/HomeHeader.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
 import { RouterLink } from 'vue-router'
-import Logo from "@/assets/images/transparent-black-logo.png"
+import Logo from '@/assets/images/transparent-black-logo.png'
 
 const prop = defineProps<{
   link: string
@@ -15,11 +15,10 @@ const click = () => {
 </script>
 
 <template>
-  <header className="w-full h-25 flex justify-between items-center px-4 md:px-8 lg:px-12">
+  <header class="w-full h-25 flex justify-between items-center px-4 md:px-8 lg:px-12">
     <figure class="w-32 md:w-40 h-auto" @click="click">
-      <img :src="Logo" class="w-full h-full object-fit" alt="Ticket ease logo"/>
+      <img :src="Logo" class="w-full h-full object-fit" alt="Ticket ease logo" />
     </figure>
-
 
     <RouterLink
       :to="prop.link"
